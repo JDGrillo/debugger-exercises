b339df9ed5da526e2f424d1ba2733cd3a624656d
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-10-15 20:05:49 -0500

    removing debugger

--- bug6/index.js
+++ bug6/index.js
@@ -3,7 +3,6 @@
 //   given a list of transactions, will generate an HTML
 //   string representing the transactions
 //*******************************************************
-debugger;
 function renderTransactions(transactions) {
 	var finalHTML = '<div class="buffer">TRANSACTIONS</div>';
 	// transaction needs to be passed through callback function
